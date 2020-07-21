4b77182ed6fcb0dae107bffcd65915f6fb25dca3
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-21 13:36:20 -0700

    create a Parcel class to create parcel objects with dimensions and weight properties, and functionality to calculate volume and cost to ship
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

---
+++ Parcels/Models/Parcel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+namespace Parcels.Models
+{
+  public class Parcel
+  {
+    public int Length {get; set;}
+    public int Height {get; set;}
+    public int Width{get; set;}
+    public int Weight{get; set;}
+
+    public Parcel(int length, int height, int width, int weight)
+    {
+      Length = length;
+      Height = height;
+      Width = width;
+      Weight = weight;
+    }
+    
+    public int Volume()
+    {
+      return Length * Height * Width;
+    }
+
+    public int CostToShip()
+    {
+      return this.Volume() * 5 + Weight * 3;
+    }
+
+  }
+}
